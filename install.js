8b983714c5287fc45ac430030d8f42c7f9aaeb5c
pinokio <pinokio@localhost>
2026-01-09 02:48:45 +1300

    Add 5 files, Update 2 files

--- install.js
+++ install.js
@@ -5,17 +5,19 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone git clone https://huggingface.co/spaces/YupengZhou/StoryDiffusion app",
+          "git clone https://huggingface.co/spaces/YupengZhou/StoryDiffusion app",
         ]
       }
     },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
+          "uv tool install hf",
+          "git xet install",
+          "hf download YupengZhou/StoryDiffusion --repo-type=space",
           "uv pip install gradio devicetorch",
           "uv pip install -r requirements.txt"
         ]
@@ -29,10 +31,10 @@ module.exports = {
         params: {
           venv: "env",                // Edit this to customize the venv folder path
           path: "app",                // Edit this to customize the path to start the shell from
-          // flashattention: true   // uncomment this line if your project requires flashattention
-          // xformers: true   // uncomment this line if your project requires xformers
-          // triton: true   // uncomment this line if your project requires triton
-          // sageattention: true   // uncomment this line if your project requires sageattention
+          flashattention: true,   // uncomment this line if your project requires flashattention
+          xformers: true,   // uncomment this line if your project requires xformers
+          triton: true,   // uncomment this line if your project requires triton
+          sageattention: true   // uncomment this line if your project requires sageattention
         }
       }
     },
